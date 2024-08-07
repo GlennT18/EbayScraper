e3fe6ba408f0c117d5f28b5901de96afd0f37c1c
Glenn Todd <grt2309@rit.edu>
2024-08-07 14:58:07 -0400

    added user inputs for the links and desired price

--- driver.py
+++ driver.py
@@ -3,11 +3,14 @@ from prices import *
 
 if __name__ == "__main__":
     #returns a list of all the linnks on an Ebay site
-    links = getSiteContents()
+    userInputURL = input("Enter Ebay link here:\n")
+    userInputPrice = input("Enter price here ($10.00 = 1000)\n")
+    userInputPrice = int(userInputPrice)
+    links = getSiteContents(userInputURL)
     
     #getPrices goes through the list of links
     #and only returns the links that are under the value of
     #the parameter
-    links = getPrices(links, 1000)
+    links = getPrices(links, userInputPrice)
     for link in links:
         print(link)
