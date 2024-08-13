6bfe458d898f89f842f9e51604aa5f0fb12106bf
Glenn Todd <grt2309@rit.edu>
2024-08-13 11:45:27 -0400

    added splashscreen for loading while background process runs, centered elements horizonatally but not vertically yet

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-from json.tool import main
 from nicegui import ui
 import asyncio
 from driver import *
 from getSite import *
 from info import *
 
+ui.page_title("EbayScraper")
+
 with ui.row().classes('w-full border'):
     ui.space()
-    ui.html('Welcome to <strong>Ebay Scrapper</strong>').style('font-size:150%')
+    ui.html('Welcome to <strong>Ebay Scraper</strong>').style('font-size:150%')
     ui.space()
 
 with ui.column().classes('w-full no-wrap'):
     #user input section
     linkInput = ui.input('Enter Link:').classes('w-1/4')
     priceInput = ui.input("Enter desired price(ex. $10.00):").classes('w-1/4')
-    ui.button('Run Search', on_click= lambda: go())
+
+    #start async here and wait until table has been updated
+    ui.button('Run Search', on_click= lambda: show_splashscreen())
 
     resultLabel = ui.label('Results:')
     columns = [
@@ -24,7 +27,7 @@ with ui.column().classes('w-full no-wrap'):
         {'name': 'img', 'label': 'Image', 'field': 'imgField', 'align': 'left'}
     ]
 
-    table = ui.table(columns=columns, rows=[], row_key='name').classes('w-full no-wrap')
+    table = ui.table(columns=columns, rows=[], row_key='name').classes('w-full border')
     #to update this to display URL - remove click here and put {{props.value}}
     table.add_slot('body-cell-link', '''
     <q-td :props="props">
@@ -38,6 +41,15 @@ with ui.column().classes('w-full no-wrap'):
     ''')
 
 ui.run()
+
+async def show_splashscreen():
+    with ui.dialog(value=True) as dialog, ui.card().classes('w-1/4 h-1/4'):
+        with ui.column().classes('w-full items-center align-middle'):
+            ui.spinner(size="75px")
+            ui.html('<strong>loading...</strong>').style('font-size:100%')
+        await asyncio.sleep(5) # <- logic goes in here
+        go()
+        dialog.close()
 
 def go():
     #clear the table to remove old search results
@@ -63,3 +75,5 @@ def go():
         table.add_rows({'priceField': prices[counter], 'titleField': titles[counter], 'linkField': x, 'imgField': imgs[counter]})
         counter += 1
     resultLabel.set_text('%3d Results:' % (counter))
+
+
